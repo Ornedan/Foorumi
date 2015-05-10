dd73d497da15bd2ab566786f81c2a1e17ae1f9c0
Heikki Aitakangas <ornedan@gmail.com>
2015-05-10 21:10:35 +0300

    Exercise 53 done

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -1,3 +1,22 @@
-FoorumApp.controller('ShowMessageController', function($scope, $routeParams, Api){
-  // Toteuta kontrolleri tähän
+FoorumApp.controller('ShowMessageController', function ($scope, $routeParams, Api) {
+    // Toteuta kontrolleri tähän
+    function update() {
+        Api.getMessage($routeParams.id).success(function (message) {
+            $scope.message = message;
+        });
+    }
+    
+    $scope.addReply = function () {
+        Api.addReply($scope.newReply, $scope.message.id).success(function (reply) {
+            // Immediately show our new reply, but then refresh full reply list
+            // from server - someone else might have also replied in the
+            // meanwhile.
+            $scope.message.Replies.push(reply);
+            update();
+        });
+        
+        $scope.newReply.content = "";
+    };
+    
+    update();
 });
