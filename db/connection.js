6208aa6b8cfe5f8dab1058616a217acbe0e97833
Heikki Aitakangas <ornedan@gmail.com>
2015-05-10 22:51:03 +0300

    Sovellus Herokuun

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,17 @@
 var Sequelize = require('sequelize');
 
+/*
 var sequelize = new Sequelize('foorumi', '', '', {
   dialect: 'sqlite',
   storage: 'db/database.sqlite'
 });
+*/
+
+var sequelize = new Sequelize('postgres://xwqkgndfrksljn:cVteesk6GSSxdHgyaBp39KXgZA@ec2-23-21-96-129.compute-1.amazonaws.com:5432/deetq662m406se', {
+  dialect: 'postgres',
+  protocol: 'postgres'
+});
+
 
 module.exports = {
   DataTypes: Sequelize,
